8ba64f773dc21aa2fb127e39f984796e9e773f9b
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2017-04-29 14:13:38 -0600

    'Entidades'

---
+++ src/main/java/com/entities/Animal.java
@@ -0,0 +1,7 @@
+package com.entities;
+
+/**
+ * Created by Ricardo on 29/4/2017.
+ */
+public class Animal {
+}
---
+++ src/main/java/com/entities/Piece.java
@@ -0,0 +1,25 @@
+package com.entities;
+
+/**
+ * Created by Ricardo on 29/4/2017.
+ */
+public class Piece extends Entity {
+    private int pieceId;
+    //agregar Layer
+
+    //public void addDrawingLayer(Layer drawingLayer){}
+
+    public void deleteDrawingLayer()
+    {
+
+    }
+
+    public int getPieceId(){
+
+    }
+
+    public void setPieceId()
+    {
+
+    }
+}
