b119086459f8f027bc849dda185680889afd4164
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2017-06-19 07:58:42 -0600

    trabajando en la búsqueda

--- src/main/java/com/morpho/server/SearchEngine.java
+++ src/main/java/com/morpho/server/SearchEngine.java
@@ -1,8 +1,18 @@
 package com.morpho.server;
 
+import com.mongodb.DBCursor;
+import com.mongodb.client.FindIterable;
 import com.morpho.MorphoApplication;
 import org.json.simple.JSONObject;
+import org.bson.Document;
 
+//import be.tarsos.lsh.Index;
+//import be.tarsos.lsh;
+
+import java.util.Collections;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +26,8 @@ public class SearchEngine {
 
     Map<String, Integer> propertiesKeys;
 
+    MessageDigest digester;
+
     public SearchEngine(){
         /*********Irvin***********/
         //Until we identify other properties throught the set up of the rest
@@ -27,6 +39,16 @@ public class SearchEngine {
         propertiesKeys.put("author",1);
         propertiesKeys.put("part",2);
         propertiesKeys.put("color",3);
+
+        /*********Ricardo***********/
+        try
+        {
+            digester = MessageDigest.getInstance("MD5");
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            //log
+        }
     }
 
     /**
@@ -65,8 +87,47 @@ public class SearchEngine {
         return code;
     }
 
-    /*public List<JSONObject> searchSimilarComposition( JSONObject composition )
+    public void addSearchIdToPart(JSONObject part)
     {
+        digester.update(part.toJSONString().getBytes());
+        byte[] hashValue = digester.digest();
 
+        part.put("searchId", hashValue);
+    }
+
+    public void addSearchIdToComposition(JSONObject composition)
+    {
+        List<Integer> searchId = new ArrayList<Integer>();
+        for (Object part : composition.values())
+        {
+            searchId.add(((JSONObject)part).getInt("searchId"));
+        }
+
+        Collections.sort(searchId);
+
+        composition.put("searchId", searchId);
+    }
+
+    /*public List<JSONObject> searchSimilarCompositions(JSONObject composition, int pageNum)
+    {
+        List<JSONObject> results = new List<JSONObject>();
+
+        addSearchIdToComposition(composition);
+
+        DBCursor partialResults;
+        JSONArray searchCriteria = composition.getJsonArray("searchId");
+        int closeness = searchCriteria.size();
+        do {
+            partialResults = MorphoApplication.DBA.search("composition",
+                    "{{searchId : {$slice : " + closeness + "}} : " + searchCriteria.toJsonString() + "}");
+            int i = 0;
+            while (i < 10 && partialResults.hasNext())
+            {
+                results.add(partialResults.next());
+                i++;
+            }
+            closeness--;
+            searchCriteria = searchCriteria.subList(0, closeness);
+        } while (results.size() < 10 && closeness >= 0)
     }*/
 }
