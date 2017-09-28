03bc7461b5c2dba2bad599bbc4bbab0ce6549f28
Irvin Umaña <irvin.umana@ucr.ac.cr>
2017-09-27 23:42:31 -0600

    fixed log in and log out behavior for facebook log in and google

--- src/main/resources/assets/js/homepage.js
+++ src/main/resources/assets/js/homepage.js
@@ -838,52 +838,6 @@ function modifySearchButton()
 }
 
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  var id_token = googleUser.getAuthResponse().id_token; // The ID token you need to pass to your backend:
-  //console.log("ID Token: " + id_token);
-  onSignInGmail();
-
-  		$.ajax({
-  			url: "/methods/sendTokenGmail",
-  			type: 'POST',
-  			data: JSON.stringify({
-  			    accessToken: id_token
-  			}),
-  			contentType: "text/plain",
-  			success:function(data, textStatus, jqXHR){
-  				console.log("token sent")},
-  			error:function(jqXHR, textStatus, errorThrown ){
-  				console.log(errorThrown);
-  			}
-  		});
-  		Cookies.set("accessToken", id_token);
-  	}
-
-/*
-  var xhr = new XMLHttpRequest();
-  //xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
-  xhr.open('POST', '/methods/sendToken');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  //xhr.setRequestHeader('Content-Type', 'multipart/form-data');
-  xhr.onload = function() {
-    console.log('Signed in as: ' + xhr.responseText);
-  };
-  xhr.send('idtoken=' + id_token);
-*/
-
-function signOut() {
-    onSignOutGmail();
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-    document.location.href = '/';
-  }
 
 
 
--- src/main/resources/assets/js/login.js
+++ src/main/resources/assets/js/login.js
@@ -47,6 +47,7 @@ var auth_status_change_callback = function(response) {
 		Cookies.set("accessToken", response.authResponse.accessToken);
 
 		$("#pieceEditorLink").show();
+		$("#LogButtonText").html("Log Out");
 		$("#profileLink").show().attr('href', "/profile?access_token="
 		+ response.authResponse.accessToken + "&userId="
 		+ response.authResponse.userID);
@@ -65,7 +66,59 @@ var logout_event = function(response)
     console.log("logout_event");
     $("#pieceEditorLink").hide();
     $("#profileLink").hide();
+    $("#LogButtonText").html("Log In");
     console.log(response.status);
     document.location.href = '/';
 }
 
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  var id_token = googleUser.getAuthResponse().id_token; // The ID token you need to pass to your backend:
+  //console.log("ID Token: " + id_token);
+  onSignInGmail();
+
+  		$.ajax({
+  			url: "/methods/sendTokenGmail",
+  			type: 'POST',
+  			data: JSON.stringify({
+  			    accessToken: id_token
+  			}),
+  			contentType: "text/plain",
+  			success:function(data, textStatus, jqXHR){
+  				console.log("token sent")},
+  			error:function(jqXHR, textStatus, errorThrown ){
+  				console.log(errorThrown);
+  			}
+  		});
+  		Cookies.set("accessToken", id_token);
+  		$("#pieceEditorLink").show();
+  		$("#LogButtonText").html("Log Out");
+  	}
+
+/*
+  var xhr = new XMLHttpRequest();
+  //xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+  xhr.open('POST', '/methods/sendToken');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  //xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+  xhr.onload = function() {
+    console.log('Signed in as: ' + xhr.responseText);
+  };
+  xhr.send('idtoken=' + id_token);
+*/
+
+function signOut() {
+    onSignOutGmail();
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+    $("#pieceEditorLink").hide();
+    $("#LogButtonText").html("Log in");
+    document.location.href = '/';
+  }
