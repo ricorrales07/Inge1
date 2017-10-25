04518751698d3f86c5a39688838ec6b11656b9f5
Irvin Umaña <irvin.umana@ucr.ac.cr>
2017-10-25 06:53:56 -0600

    fixing style

--- src/main/resources/assets/js/searchBase.js
+++ src/main/resources/assets/js/searchBase.js
@@ -1,4 +1,9 @@
 
-for(var i= 0; i< 50; i++){
-	$( ".resultsCardSpace" ).clone().appendTo( ".resultsBlockRow" );
+
+$(document).ready(function() {
+	var original = $( ".resultsCardSpace" ).clone();
+      for(var i= 0; i< 15; i++){
+	original.clone().delay(1000).appendTo( ".resultsBlockRow" );
 }
+});
+
